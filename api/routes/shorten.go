90d629546324b7f2a0e7b9a1bcd6c55e3036c2b6
Thiago Cardoso <intelxd@gmail.com>
2022-08-19 22:17:24 -0300

    scaffolds basic files and folders; implements initial fiber app

---
+++ api/routes/shorten.go
@@ -0,0 +1,46 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short`
+	Expire      time.Duration `json:"expire"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expire          time.Duration `json:"expire"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// implement rate limit
+
+	// check if the input is an actual url
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "You can't access this"})
+	}
+
+	// enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+}
