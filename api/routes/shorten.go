2c09625df57498a4e2e67f564f1c42227d0c5c87
Thiago Cardoso <intelxd@gmail.com>
2022-08-19 22:53:11 -0300

    implements shortner's rate-limit strategy

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	"strconv"
+
+	"github.com/thiagocardoso1988/go-url-shortener/api/database"
 	"github.com/thiagocardoso1988/go-url-shortener/api/helpers"
 
+	"os"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -30,6 +35,24 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// implement rate limit
+	r2 := database.CreateClient((1))
+	defer r2.Close()
+
+	value, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	}
+
+	value, _ = r2.Get(database.Ctx, c.IP()).Result()
+	valueInt, _ := strconv.Atoi(value)
+
+	if valueInt <= 0 {
+		limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error":            "Rate limit exceeded",
+			"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+		})
+	}
 
 	// check if the input is an actual url
 	if !govalidator.IsURL(body.URL) {
@@ -43,4 +66,7 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+	r2.Decr(database.Ctx, c.IP())
+
+	return nil
 }
